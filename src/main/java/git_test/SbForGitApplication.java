14c538ece6342ab6191231ff483eb55f4dea4f09
vishwa2033 <703406946@genpact.com>
2024-12-23 18:01:27 +0530

    Created SbForGit project and it is an initial commit.

---
+++ src/main/java/git_test/SbForGitApplication.java
@@ -0,0 +1,13 @@
+package git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbForGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git_test/SbForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package git_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbForGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
